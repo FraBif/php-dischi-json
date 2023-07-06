757add866623388803c24ddeef05a6ed824da029
FraBif <francescobif@hotmail.it>
2023-07-06 16:58:15 +0200

    added style css

--- index.php
+++ index.php
@@ -8,16 +8,17 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.4.0/axios.min.js"></script>
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <link rel="stylesheet" href="./styles/style.css">
 
 </head>
 <body>
     <div id="app">
         <header class="col-12 d-flex justify-start">
-            <img src="./img/spotilogo.png" alt="">
+            <img src="./img/spotilogo.png" alt="logo" class="logo">
         </header>
         <main>
         <article class="disc-card" v-for="(disc, index) in discList" :key="index">
-                <div class="poster-wrapper">
+        <div class="poster-wrapper">
                     <img :src="disc.poster" :alt="disc.title">
                 </div>
                 <div class="disc-info">
@@ -29,6 +30,6 @@
         </main>
     </div>
 
-    <script src="./script/main.js"></script>
+ <script src="./script/main.js"></script>
 </body>
 </html>
